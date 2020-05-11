69cb3d73576d71e444321fc32c01ece42d30a14c
Richard Russell <russell.ajax@gmail.com>
2020-05-11 10:18:21 +0700

    Add files

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io;
+use std::process;
+
+fn main(){
+	println!("Please enter the first number: ");
+	let mut first = String::new();
+	io::stdin().read_line(&mut first);
+	let a:u32;
+	
+	match first.trim().parse(){
+		Ok(val) => {
+			a = val;
+		}
+	
+		Err(_err) => {
+			println!("Input not an integer!");
+			process::exit(1);
+		}
+	}
+
+	println!("Please enter the second number: ");
+	let mut second = String::new();
+	io::stdin().read_line(&mut second);
+	let b:u32;
+	
+	match second.trim().parse(){
+		Ok(val) => {
+			b = val;
+		}
+		Err(_err) => {
+			println!("Input not an integer!");
+			process::exit(1);
+		}
+	}
+	
+	let result = sum(a, b);
+	println!("{} + {} = {}", a, b, result);
+}
+
+fn sum(a: u32, b: u32) -> u32{
+	a + b
+}
